e52271f6793c4a15b760e66a505729915d01c93b
kelliaUmuhire <kellumuhire@gmail.com>
2024-08-06 11:08:14 +0200

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
 import Calculator from "./components/Calculator";
 import Todo from "./components/Todo";
+import { Link, Route, Routes } from "react-router-dom";
+import Picture from "./components/Todo/Picture";
 
 export default function App() {
   const [display, setDisplay] = useState("0");
   return (
-    <div>
+    <>
       <div className="flex justify-center items-center gap-5 mt-10">
-        <button
+        <Link
+          to="/"
           className={`py-2 px-4 rounded-full ${
             display == "0" ? "bg-gray-200" : "bg-gray-100"
           }`}
           onClick={() => setDisplay("0")}
         >
           Calculator
-        </button>
-        <button
+        </Link>
+        <Link
+          to="/todo"
           className={`py-2 px-4 rounded-full ${
             display == "1" ? "bg-gray-200" : "bg-gray-100"
           }`}
           onClick={() => setDisplay("1")}
         >
           Todo
-        </button>
+        </Link>
       </div>
-      {display == "0" ? <Calculator /> : <Todo />}
-    </div>
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+        <Route path="/todo" element={<Todo />}>
+          <Route path=":tag" element={<Picture />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
